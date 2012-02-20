3d918a584df6e5f74795b66282652b8b1e80d532
Artem <artem@veremey.net>
2012-02-20 15:58:01 -0800

    Rdoc typo

--- metadata.rb
+++ metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Artem Veremey"
 maintainer_email "artem@veremey.net"
 license          "Apache 2.0"
 description      "Installs/Configures syslog-ng"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
