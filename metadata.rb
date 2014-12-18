1a45e124c33f1252589692148c95cb8f42b139d1
David Schlenk <david.schlenk@spanlink.com>
2014-12-17 20:34:23 -0600

    bumped version just for confusion avoidance

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "artem@veremey.net"
 license          "Apache 2.0"
 description      "Installs/Configures syslog-ng"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.3.0"
+version          "1.4.0"
