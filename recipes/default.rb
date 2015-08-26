7d51cdabe5bb32b4aee39ca71e9a03da56ad5988
WaffleSouffle <wafflesouffle@gmail.com>
2015-08-26 13:59:24 +0100

    Use newer syslog-ng configuration, simpler include mechanism. Tested on ubuntu 14.04 with syslog 3.5.3-1. Moved cookbook version up to 1.5.0. Read syslog-ng config version from attribute.
    attributes/default.rb
    Replaced: sync with flush_lines, and long_hostnames with chain_hostnames.
    Added: syslog_ng_config_name.
    
    files/default/syslog-ng
    Try to handle running on ubuntu as well as CentOS by trying different binary paths and lockfiles.
    
    metadata.rb
    Moved to version 1.5.0.
    
    recipes/default.rb
    Added ruby_block to add include 'conf.d' if not present in existing syslog-ng.conf;
    definition call 'generate_syslog_ng_conf_if_missing'.
    Altered: order of service start.
    Updated: default attributes (see above).
    
    templates/default/00base.erb
    Updated: default attributes (see above).
    
    Added:
    definitions/generate_syslog_ng_conf_if_missing.rb
    templates/default/syslog-ng.conf.erb
    
    Deleted:
    files/default/syslog-ng.conf
    Replaced with template above.

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,16 +19,44 @@
 
 package "syslog-ng"
 
-cookbook_file "#{node[:syslog_ng][:config_dir]}/syslog-ng.conf" do
-  owner node[:syslog_ng][:user]
-  group node[:syslog_ng][:group]
-  mode 00640
+ruby_block 'Ensure the syslog-ng main configuration file includes configuration files in conf.d subdirectory' do
+  filepath = "#{node[:syslog_ng][:config_dir]}/#{node[:syslog_ng][:syslog_ng_config_name]}"
+
+  block do
+    newline = $/
+
+    conf_file = File.open(filepath, 'rb')
+    content = conf_file.read
+    conf_file.close
+
+    #@include 'conf.d'
+    findIncludeExpr = Regexp.new(%q{^\s*@include\s*(?:"|')[a-zA-Z\\/.-]*(?:\\/)?conf.d}, Regexp::MULTILINE)
+    match = findIncludeExpr.match(content)
+
+    if match.nil?
+      content << %q(@include 'conf.d') + newline
+
+      Tempfile.open(".#{File.basename(filepath)}", File.dirname(filepath)) do |tempfile|
+        tempfile.binmode
+        tempfile.write(content)
+        tempfile.close
+        stat = File.stat(filepath)
+        FileUtils.chown stat.uid, stat.gid, tempfile.path
+        FileUtils.chmod stat.mode, tempfile.path
+        FileUtils.mv tempfile.path, filepath
+      end
+    else
+      puts 'Include already present in syslog-ng configuration file.'
+    end
+  end
+  only_if { File.exists? filepath }
 end
 
 cookbook_file "/etc/init.d/syslog-ng" do
   owner node[:syslog_ng][:user]
   group node[:syslog_ng][:group]
   mode 00755
+  action :create_if_missing
 end
 
 directory "#{node[:syslog_ng][:config_dir]}/conf.d" do
@@ -47,7 +75,10 @@ end
 
 service "syslog-ng" do
   supports :restart => true, :status => true
-  action [ :enable, :start ]
+  action [ :enable ]
+end
+
+generate_syslog_ng_conf_if_missing do
 end
 
 template "#{node[:syslog_ng][:config_dir]}/conf.d/00base" do
@@ -56,16 +87,21 @@ template "#{node[:syslog_ng][:config_dir]}/conf.d/00base" do
   group node[:syslog_ng][:group]
   mode 00640
   variables(
-    :sync => node[:syslog_ng][:sync],
+    :flush_lines => node[:syslog_ng][:flush_lines],
     :time_reopen => node[:syslog_ng][:time_reopen],
     :log_fifo_size => node[:syslog_ng][:log_fifo_size],
-    :long_hostnames => node[:syslog_ng][:long_hostnames],
+    :chain_hostnames => node[:syslog_ng][:chain_hostnames],
     :use_dns => node[:syslog_ng][:use_dns],
-    :use_fqdn => node[:syslog_ng][:use_fqdn],
     :create_dirs => node[:syslog_ng][:create_dirs],
+    :use_fqdn => node[:syslog_ng][:use_fqdn],
     :keep_hostname => node[:syslog_ng][:keep_hostname],
     :chain_hostnames => node[:syslog_ng][:chain_hostnames],
     :global_opts => node[:syslog_ng][:global_opts]
   )
   notifies :restart, "service[syslog-ng]"
+end
+
+service "syslog-ng_start" do
+  service_name 'syslog-ng'
+  action [ :start ]
 end
